afa2d629035ca4596012e166b86477489fb5dace
g-martelly <nxt.tech@gmail.com>
2020-12-09 07:29:32 -0800

    Stuck on list button alignment

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-//make button work
+//make Add item button work
+document.getElementById("add-button").addEventListener('click', addItem)
+
 //figure out how to add a new list item to ul#current-items 
+
 //add a new li item using text from the textbox
 //implement enter button on textbox
 //
+
+
+function addItem() {
+   var textbox = document.getElementById('textbox');
+   var currentItemsList = document.getElementById('current-items');   
+    var newItem = document.createElement('li');
+    newItem.className = 'current-item';
+    newItem.textContent = textbox.value;
+    currentItemsList.appendChild(newItem);
+    textbox.clear
+}
