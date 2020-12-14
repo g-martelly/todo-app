ce445cf2e1d214b3e3a9ffda41444993a4e9155f
g-martelly <nxt.tech@gmail.com>
2020-12-13 16:04:34 -0800

    Delete item working.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //make Add item button work
-document.getElementById("add-button").addEventListener('click', addItem)
+document.getElementById("add-button").addEventListener('click', addItem);
+document.querySelector('#current-items').addEventListener('click', deleteItem);
 
 //figure out how to add a new list item to ul#current-items 
 
@@ -7,13 +8,46 @@ document.getElementById("add-button").addEventListener('click', addItem)
 //implement enter button on textbox
 //
 
+input = document.getElementById('textbox');
+
+input.addEventListener("keypress", function(e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        document.getElementById("add-button").click();
+    }
+})
+
 
 function addItem() {
-   var textbox = document.getElementById('textbox');
-   var currentItemsList = document.getElementById('current-items');   
-    var newItem = document.createElement('li');
-    newItem.className = 'current-item';
-    newItem.textContent = textbox.value;
-    currentItemsList.appendChild(newItem);
-    textbox.clear
+var textbox = document.getElementById('textbox');
+var currentItemsList = document.getElementById('current-items');   
+var currentDivItem= document.createElement('div');
+var newListElement = document.createElement('li');
+var XButton = document.createElement('button');
+   
+  newListElement.textContent = textbox.value;
+  XButton.className = 'delete-button';
+  XButton.textContent = "DONE";
+   currentDivItem.className = 'current-list-item';
+   currentItemsList.appendChild(currentDivItem);
+   currentDivItem.appendChild(newListElement);
+   currentDivItem.appendChild(XButton);
+
+   clearTextAndFocus();
+}
+
+function clearTextAndFocus() {
+    textbox.value = "";
+    textbox.focus();
+}
+function deleteItem(evt) {
+    evt = evt;
+    var currentItem = evt.srcElement;
+    var i = document.getElementById('current-items');
+    console.table(i.child);
+    console.log("this is it: " + currentItem.parentElement);
+    var el = this;
+    i.removeChild(currentItem.parentElement);
+    
+   console.table("evt is: " + evt + "targ is: " + currentItem + "this is: " + el);
 }
