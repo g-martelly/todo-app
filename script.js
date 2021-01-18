13fec7a30faec770586206d78e7171780d5c0c9f
g-martelly <nxt.tech@gmail.com>
2021-01-17 19:23:08 -0800

    Added changes from Chris' code review. Ready to start local storage.

--- script.js
+++ script.js
@@ -1,90 +1,76 @@
-document.getElementById("add-button").addEventListener('click', addItem);
-document.getElementById("clear-button").addEventListener('click', clearItems);
-var out = document.querySelector('button.delete-button');
+const input = document.getElementById('textbox');
 
-input = document.getElementById('textbox');
-
-input.addEventListener("keypress", function(e) {
-
- var textbox = document.getElementById('textbox');
-var text = textbox.value.trim();
-if (text.length == 0) {
-    textbox.value = "";
-}
-    if (e.keyCode === 13 && input.value != "") {
-        e.preventDefault();
-        document.getElementById("add-button").click();
-    } else if (e.keyCode == 13 && input.value == ""){
-        e.preventDefault();
-    }
+const completeItem = (e) => {
+    let currentItem = e.target;
    
+    currentItem.classList.toggle("done"); 
+}
+
+const createTodoListItem = (todo) => { 
+    const todoListItem = document.createElement('li');
+    todoListItem.textContent = todo.text;
+
+    todoListItem.addEventListener("click", e => { 
+        completeItem(e); 
+    })
+
+    return todoListItem; 
+}
+
+
+const createDeleteButton = (todo, todoDiv) => { 
+    const deleteButton = document.createElement('span');
+    deleteButton.className = 'x';
+    deleteButton.innerHTML = "";
+
+    deleteButton.addEventListener('click', () => { 
+        todoDiv.remove(); 
+    })
+
+
+    return deleteButton; 
+}
+
+const  addTodo = () => {    
+    const currentItemsList = document.getElementById('current-items');   
+    const todoDiv = document.createElement('div');
+    todoDiv.className = 'current-list-item';
+    
+    const todo = { 
+        id: Math.random(), 
+        text: input.value, 
+        isCompleted: false, 
+    }
+
+    const todoListItem = createTodoListItem(todo); 
+    const deleteButton = createDeleteButton(todo, todoDiv);
+
+
+       todoDiv.appendChild(todoListItem);
+       todoDiv.appendChild(deleteButton);
+       currentItemsList.appendChild(todoDiv);
+    
+    
+
+       input.value = "";
+       input.focus();
+    }
+
+
+document.getElementById("add-button")
+.addEventListener('click', addTodo);
+
+document.addEventListener("keydown", e => { 
+    if (e.key === "Enter" && input.value != "") {
+        e.preventDefault();
+        addTodo();
+    }
 })
 
-function addItem() {
-var addButton = document.getElementById('add-button');
 
-var currentItemsList = document.getElementById('current-items');   
-var currentDivItem= document.createElement('div');
-var newListElement = document.createElement('li');
-var XButton = document.createElement('span');
-   
-  newListElement.textContent = textbox.value;
-  XButton.className = 'x';
-  XButton.style.display = 'none';
-  XButton.innerHTML = "";
-   currentDivItem.className = 'current-list-item';
-   currentItemsList.appendChild(currentDivItem);
-   currentDivItem.appendChild(newListElement);
-   currentDivItem.appendChild(XButton);
 
-  const buttons =  document.querySelectorAll('#current-items div.current-list-item span.x');
-  for (const button of buttons) {
-    button.addEventListener('click', deleteItem);
-  }
 
-  const listItems =  document.querySelectorAll('#current-items div.current-list-item li');
-  for (const listItem of listItems) {
-    listItem.addEventListener('click', completeItem);
-  }
-
-   clearTextAndFocus();
-}
-
-function clearTextAndFocus() {
-    textbox.value = "";
-    textbox.focus();
-}
-function deleteItem(evt) {
-    evt = evt;
-    var currentItem = evt.srcElement;
-    var i = document.getElementById('current-items');
-    var el = this;
-    i.removeChild(currentItem.parentElement);
-
-}
-
-function completeItem(evt) {
-    evt = evt;
-    var currentItem = evt.srcElement;
-   
-if (currentItem.className != 'done') {
-    currentItem.className = "done";
-    var xButton = currentItem.nextElementSibling;
-    xButton.style.display = 'initial';
-    
-}  else {
-    currentItem.className = ""; 
-    var xButton = currentItem.nextElementSibling;
-    xButton.style.display = 'none';
-}
-
-}
-
-function clearItems() {
-
-    const listItems =  document.querySelectorAll('#current-items div.current-list-item li.done');
-    for (const listItem of listItems) {
-     listItem.parentNode.parentNode.removeChild(listItem.parentNode);
-    }
-
-}
+document.getElementById("clear-button")
+.addEventListener('click', () => { 
+    document.getElementById('current-items').innerHTML = '';   
+});
